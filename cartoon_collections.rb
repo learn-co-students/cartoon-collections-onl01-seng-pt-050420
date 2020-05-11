439cd1f0b8f18ba7c0ed3042c81714af24441e86
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-11 18:43:09 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(collection)
+  
+  i = 0 
+  while i < collection.length 
+    puts "#{i+1}. #{collection[i]}"
+    i += 1 
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(collection)
+  new_collection = []
+  collection.map{ |element| 
+    x = element.capitalize()
+    x << "!"
+    new_collection << x
+    
+  }
+  new_collection
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any?{ |word|
+    word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacc)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.map{ |cheese|
+   if snacc.include?(cheese)
+     return cheese
+   else
+     return nil
+   end
+  }
 end
+
+find_the_cheese(["tomato soup", "cheddar", "oyster crackers", "gouda"])
