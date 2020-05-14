11dad7eaeb5ec86c57597b4da637fece402ea374
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-14 03:21:49 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  little_people = []
+  array.each_with_index do |name, index|
+  puts "#{index + 1}. #{name}"
+ end
+ little_people
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+
+def summon_captain_planet(array)
+  array.collect do |yell|
+  yell.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls(array)
+  array.any? {|string| string.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  array.detect {|snack| cheese_types.include?(snack)}
+  
+ end
