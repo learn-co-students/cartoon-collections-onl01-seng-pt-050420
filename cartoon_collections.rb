3f16deb6ccd3dba8eec79f10ed39b8b9d0feacbd
Dustin Blanton <dustinblntn@gmail.com>
2020-05-14 21:33:42 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,44 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(array)
+  i = 0 
+  while i < array.length 
+  array.each_with_index do |name, index|
+    puts "#{index + 1} #{name}"
+  end
+   i += 1 
+ end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(array)
+  array.map {|p| p.capitalize + "!"}
+
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+short_words = ["puff", "go", "two"]
+
+assorted_words = ["two", "go", "industrious", "bop"]
+
+def long_planeteer_calls(array)
+  array.any? {|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+snacks = ["crackers", "gouda", "thyme"]
+
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+
+ingredients = ["garlic", "rosemary", "bread"]
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  array.detect {|i| i == "cheddar" or i == "gouda" or i == "camembert"}
+  
+
+  
 end
+find_the_cheese(soup)
